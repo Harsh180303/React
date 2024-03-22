96d72b86c58c85d475e114b7a34b4e2ec957a69a
Harsh180303 <harshchourasiya6261@gmail.com>
2024-03-23 01:33:01 +0530

    using api

--- 01_Inception/src/components/Body.js
+++ 01_Inception/src/components/Body.js
@@ -1,36 +1,72 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+  const [listOfRestaurants, setListOfRestaurants] = useState([])
+ 
+  const [filteredRestaurant, setFilteredRestaurant] = useState([])
 
-    return (
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {            //Ye bad mai render hota hai
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const json = await data.json();
+    console.log(json)
+    // setListOfRestaurants(json.data.cards[10].card.card.cities[16].link)    // review later
+
+    // Optinal Chaining
+    setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+  
+  // Conditional Operation 
+    return listOfRestaurants.length === 0 ? (
+      <Shimmer />
+    ) :(
       <div className="body">
-        <div>
           <div className="filter">
+            <div>
+              <input
+                className="search"
+                type="search"
+                placeholder="Search..."
+                value={searchText}
+                onChange={(e) => {
+                  setSearchText(e.target.value)
+                }}
+              /> 
+              <button onClick={() => {
+                // Filter the restaurant cards and update the UI
+                // console.log(searchText)
+                const filteredRestaurants = listOfRestaurants.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+
+                setFilteredRestaurant(filteredRestaurants)
+              }}>Search</button>
+            </div>
+            
             <button
               className="filter-btn"
               onClick={() => {
                 const filteredList = listOfRestaurants.filter(
-                  (res) => res.info.avgRating >= 4.5
+                  (res) => res.info.avgRating >= 4
                 )
-                setListOfRestaurants(filteredList)
+                setFilteredRestaurant(filteredList)
               }}
               
             >
               Top Rated
             </button>
           </div>
-          <input
-            className="search"
-            type="search"
-            placeholder="Search..."
-          ></input>
-        </div>
+
         <div className="res-container">
-          {listOfRestaurants.map((restaurant) => (
+          {filteredRestaurant.map((restaurant) => (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           ))}
         </div>
--- 01_Inception/src/components/Header.js
+++ 01_Inception/src/components/Header.js
@@ -1,20 +1,49 @@
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constents"
 
 const Header = () => {
+
+  const [btnName, setBtnName] = useState("Login");
+
+  
     return (
       <div className="header">
         <div className="logo-container">
-          <img
-            className="logo"
-            src= {LOGO_URL}
-          />
+          <img className="logo" src={LOGO_URL} />
         </div>
         <div className="nav-items">
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">Cart</a></li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+            <li>
+              <a href="#">Cart</a>
+            </li>
+
+            <button
+              className="login"
+              onClick={() => {
+                  btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+                // console.log(btnName);   //check
+              }}
+            >
+              {btnName}
+            </button>
+
+            {/* <button
+              className="login"
+              onClick={() => {
+                setBtnName("Logout")
+              }}
+            >
+              {btnName}
+            </button> */}
           </ul>
         </div>
       </div>
